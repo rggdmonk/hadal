057edbaa44898e5a4c8411db9ad07690edaddb09
rggdmonk <32217886+rggdmonk@users.noreply.github.com>
2023-11-10 12:07:56 +0300

    add black and pymdownx.magiclink to dependencies, increase line length in docstring generation, update api_reference

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -12,7 +12,7 @@ for path in sorted(Path("hadal").rglob("*.py")):
 
     module_path = path.relative_to("hadal").with_suffix("")
     doc_path = path.relative_to("hadal").with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+    full_doc_path = Path("api_reference", doc_path)
 
     parts = tuple(module_path.parts)
 
@@ -34,5 +34,5 @@ for path in sorted(Path("hadal").rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("api_reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
